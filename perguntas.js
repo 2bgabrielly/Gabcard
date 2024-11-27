c6e9a4ed4212b76750ed5d2ecd228bc9a5829198
2bgabrielly <00001102420645sp@al.educacao.sp.gov.br>
2024-11-26 23:54:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Para que serve o Python?',
+    'O Python é usado para diversas aplicações, como automação e análise de dados.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual país tem Paris como capital?',
+    'A França tem Paris como sua capital.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um método?',
+    'Um método é uma função associada a um objeto em programação.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se escreve "tchau" em Inglês?',
+    'Tchau em inglês é "bye".'
+)
